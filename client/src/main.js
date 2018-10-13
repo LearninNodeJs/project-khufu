6647bd96c97e010ca28e6672c8036e174ac9b891
mtotodev05 <mtotodev05@gmail.com>
2018-10-13 18:45:51 +0300

    store initialized with router sync

--- client/src/main.js
+++ client/src/main.js
@@ -5,15 +5,18 @@ import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
-
+import {sync} from 'vuex-router-sync';
+import {store} from './store'
 Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
+sync(store,router);
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 });
